5f87c1ae3f822983031551831c79fbf226e3034e
Harshal Raverkar <harshal.raverkar@gmail.com>
2018-12-20 00:44:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication10
+{
+    // Delegate are type safe and the function pointer means pointing the function which have
+    // same return type and same type of parameter ..........
+    
+    public delegate void HelloFunctionDelegate(string Message);
+    
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var helloFunctionDelegate = new HelloFunctionDelegate(Hello);
+            helloFunctionDelegate("Hello Harshal Raverkar How Are you");
+        }
+
+        private static void Hello(string strMessage)
+        {
+            Console.WriteLine(strMessage);
+        }
+    }
+}
